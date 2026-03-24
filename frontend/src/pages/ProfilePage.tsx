be19e273e5d7139adf6792d491dacb33049bbac1
User-ba-oss <bethelfekadu863@gmail.com>
2026-03-24 13:09:41 -0700

    Edit profile

---
+++ frontend/src/pages/ProfilePage.tsx
@@ -0,0 +1,11 @@
+const ProfilePage = () => {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <p>Name: Sd</p>
+      <p>Email: stud@gmail.com</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
